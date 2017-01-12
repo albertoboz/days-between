bdb3c2043dedd6e43ac6967bed74076a9b7733fb
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-01-12 10:33:20 +0100

    Update daysbetween.py

--- daysbetween.py
+++ daysbetween.py
@@ -2,8 +2,6 @@ daysOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def isLeapYear(y1,y2):
 
-
-
     if y1 % 400 == 0 or y2 % 400 == 0:
         return True
     if y1 % 100 == 0 or y2 % 100 == 0:
@@ -14,14 +12,7 @@ def isLeapYear(y1,y2):
         return False
 
 
-    ##
-    # Your code here. Return True or False
-    # Pseudo code for this algorithm is found at
-    # http://en.wikipedia.org/wiki/Leap_year#Algorithm
-    ##
-
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
-
 
     totalDays = 0
 
@@ -33,8 +24,6 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
         totalDays = totalDays + daysOfYear
         y2 = y2 - 1
 
-
-
     daysLefty1 = 0
     i = 0
 
@@ -45,9 +34,7 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
 
         daysLefty1 = daysLefty1 + daysOfMonths[i-1]
 
-
         i = i + 1
-
 
     daysLefty2 = 0
     e = 0
@@ -61,11 +48,8 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
 
         e = e + 1
 
-
-
     days = totalDays + daysLefty2 + d2 - daysLefty1 - d1
 
     return days
-
 
 print (daysBetweenDates(1987, 3, 20, 2016, 6, 8))
